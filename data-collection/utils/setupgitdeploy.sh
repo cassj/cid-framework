d18fcbbe087d8f39d706b5711ffc079246b9b228
Caroline Johnston <cjaws@amazon.com>
2023-12-10 22:22:28 +1300

    aaaaagh

--- data-collection/utils/setupgitdeploy.sh
+++ data-collection/utils/setupgitdeploy.sh
@@ -32,9 +32,10 @@ done
 # set up the pre-push hook to sync the template bucket before every git push
 cat << EOF > .git/hooks/pre-push
 #!/bin/sh
+
 # Doesn't seem like you can sync to a prefix, so fudge it so I don't have to change the yaml cfn files
 mkdir -p upload/cfn/
-ln -s ../../data-collection/deploy upload/cfn/data-collection
+ln -s ../../data-collection/deploy/ upload/cfn/data-collection
 aws s3 sync upload/ s3://cid-datacollection-templates000826210026/
 rm -rf upload
 exit 0
