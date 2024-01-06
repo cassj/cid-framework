32a604d1384f303ff8f8cf4d0ef045773e0e4263
Caroline Johnston <cjaws@amazon.com>
2024-01-06 14:16:29 +1300

    separate roles

--- data-collection/utils/setupgitdeploy.sh
+++ data-collection/utils/setupgitdeploy.sh
@@ -5,15 +5,6 @@ usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
 
 while getopts "hu:b:r:s:" arg; do
   case $arg in
-    u) # Specify Github Username
-      USER=${OPTARG}
-      ;;
-    r) # Specify Github Repository name (cid-framework)
-      REPO=${OPTARG}
-      ;;
-    b) # Specify Github Branch (deploy)
-      BRANCH=${OPTARG}
-      ;;
     s) # Specify S3 bucket for nested templates
       BUCKET=${OPTARG}
       ;;
@@ -24,10 +15,7 @@ while getopts "hu:b:r:s:" arg; do
   esac
 done
 
-[[ -z $USER ]] && usage
 [[ -z $BUCKET ]] && usage
-[[ -z $BRANCH ]] && BRANCH="deploy"
-[[ -z $REPO ]] && REPO="cid-framework"
 
 # set up the pre-push hook to sync the template bucket before every git push
 cat << EOF > .git/hooks/pre-push
